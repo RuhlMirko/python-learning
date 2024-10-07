a3194e2deed0dfd667dd13c8f11217bd3059ad68
Mirko <ruhlmirkojoel@gmail.com>
2024-10-07 12:57:17 -0300

    Updated day 4 file

--- day 4/main.py
+++ day 4/main.py
@@ -1,7 +1,7 @@
 
 
 print("Welcome back")
-user_input = input("Type add, show, edit or exit: ").lower().strip()
+user_input = input("Type add, show, edit, remove or exit: ").lower().strip()
 
 todo_list = []
 while True:
@@ -13,16 +13,22 @@ while True:
                     break
                 todo_list.append(todo_input)
         case "show":
-            print(todo_list)
+            for index, item in enumerate(todo_list):
+                print(index, '-', item)
         case "edit":
             print(todo_list)
             user_edit = input("Type the word you want to edit: ").capitalize()
             index = todo_list.index(user_edit)
             todo_list[index] = input("Type the new word: ")
+        case "remove":
+            for index, item in enumerate(todo_list):
+                print(index, '-', item)
+            user_pop = input("Enter the number of the todo to remove: ")
+            todo_list.pop(int(user_pop))
         case "exit":
             break
 
-    user_input = input("Type add or show: ").lower()
+    user_input = input("Type add, show, edit, remove or exit: ").lower()
 
 
 
