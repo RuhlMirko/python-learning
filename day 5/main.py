b94242288688e1bcb95350867de5615ce8aee1dd
Mirko <ruhlmirkojoel@gmail.com>
2024-10-14 20:15:53 -0300

    Day 5 update

--- day 5/main.py
+++ day 5/main.py
@@ -5,8 +5,17 @@ while True:
 
     match user_action:
         case 'add':
-            todo = input("Enter a to-do: ")
+            todo = input("Enter a to-do: ") + "\n"
+
+            file = open("todo.txt", 'r')
+            todo_list = file.readlines()
+            file.close()
+
             todo_list.append(todo)
+
+            file = open("todo.txt", 'w')
+            file.writelines(todo_list)
+            file.close()
         case 'show':
             for index, item in enumerate(todo_list):
                 row = f"{index + 1}-{item}"
@@ -15,7 +24,8 @@ while True:
             number = int(input("Number of the todo to edit: "))
             number = number-1
         case 'complete':
-            pass
+            number = int(input("Number of to-do to complete: "))
+            todo_list.pop(number-1)
         case 'exit':
             break
 
