cd139876c30c4ea8db583d65d67bd792582348f4
Mirko <ruhlmirkojoel@gmail.com>
2024-10-16 17:53:30 -0300

    Exercise in functions

---
+++ exercises/Bonus/bonus11.py
@@ -0,0 +1,14 @@
+def get_average():
+    with open('data.txt', 'r') as file:
+        content = file.readlines()
+
+    values = content[1:]
+    values = [float(i) for i in values]
+
+    average_temp = sum(values) / len(values)
+    return average_temp
+
+
+average = get_average()
+
+print(average)
